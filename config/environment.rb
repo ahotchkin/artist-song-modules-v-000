1a9fdace49ee5e3e6400acc0ce352b7005aa7762
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-04-27 00:10:17 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require_relative '../lib/concerns/findable.rb'
 require_relative '../lib/concerns/memorable.rb'
 require_relative '../lib/concerns/paramable.rb'
 
-require_relative '../lib/artist.rb'
-require_relative '../lib/song.rb'
+require_relative '../lib/artist'
+require_relative '../lib/song'
